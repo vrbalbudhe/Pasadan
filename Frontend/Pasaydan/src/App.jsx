38123d576fc25b72fb5d0988e9457c55e9d7f5f8
AditiSurve <122209521+AditiSurve17@users.noreply.github.com>
2024-09-07 16:24:27 +0530

    Sign up/Login added

--- Frontend/Pasaydan/src/App.jsx
+++ Frontend/Pasaydan/src/App.jsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Routes/Layout";
 import Homepage from "./Routes/Homepage";
+import LoginSignupPage from "./Routes/LoginSignupPage";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
+      element: <LoginSignupPage/>,
+      
+      /*path: "/",
       element: <Layout />,
       children: [
         {
           path: "/",
           element: <Homepage />,
         },
-      ],
+      ],*/
     },
   ]);
 
