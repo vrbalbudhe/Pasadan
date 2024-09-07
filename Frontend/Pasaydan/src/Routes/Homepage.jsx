ed78ecc3151a89fb972e65d12b08e6419cccc441
AditiSurve <122209521+AditiSurve17@users.noreply.github.com>
2024-09-07 23:49:08 +0530

    Rough Landing page

--- Frontend/Pasaydan/src/Routes/Homepage.jsx
+++ Frontend/Pasaydan/src/Routes/Homepage.jsx
@@ -1,8 +1,10 @@
 // import AdvertisementBar from "../Components/AdvertisementBar";
-
+import LandingPage
+ from "../Components/LandingPage";
 function Homepage() {
   return (
     <div>
+      <LandingPage/>
       {/* <AdvertisementBar /> */}
     </div>
   );
